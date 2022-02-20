2c9c3cfca32ca6a6dce472ca294f7081edfa9339
Pavanarathod <edrocullen13@gmail.com>
2022-02-20 18:55:10 +0530

    back to basics

---
+++ boolean.py
@@ -0,0 +1,2 @@
+data = input("What's you favorite color? :")
+print("you favorite color is : " + data)
---
+++ main.py
@@ -0,0 +1,8 @@
+fname = "pavan"
+lname = "Pattinson"
+
+def full_name():
+	return f"you'r full name is {fname} {lname}"
+
+
+print(full_name())
